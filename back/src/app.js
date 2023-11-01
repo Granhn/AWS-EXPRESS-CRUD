540aa16248a05a05d0469b41c227de41715810b0
Diego Diaz Fuenzalida <diegodiazfuenzalida@MacBook-Air-de-Diego.local>
2023-11-01 17:22:32 -0300

    Hola jeje

--- back/src/app.js
+++ back/src/app.js
@@ -6,6 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000
 app.use(cors());
 app.use(router)
+app.get('/', (req, res) => {
+    res.send('Hola wawa linda👩🏽‍🔬')
+})
 
 
 app.listen(PORT, () => console.log(`Corriendo en el puerto ${PORT}`))
